a34d040e3cd1c9b22e2b534d12d2708700717cf0
Arnav Bhatnagar <arnavbhatnagar2007@gmail.com>
2025-10-04 02:41:47 +0530

    new commit

--- Day39_01.c
+++ Day39_01.c
@@ -19,3 +19,47 @@ Output 2:
 True
 
 */
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    int R, C;
+    bool are_distinct = true;
+
+    printf("Enter the number of rows (R) and columns (C): ");
+    if (scanf("%d %d", &R, &C) != 2 || R <= 0 || C <= 0) return 1;
+
+    if (R != C) {
+        printf("Output:\nFalse\n");
+        return 0;
+    }
+
+    int matrix[R][C];
+    
+    printf("Enter the elements of the %d x %d matrix:\n", R, C);
+    for (int i = 0; i < R; i++) {
+        for (int j = 0; j < C; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    int diagonal_elements[R];
+    for (int i = 0; i < R; i++) {
+        diagonal_elements[i] = matrix[i][i];
+    }
+    
+    for (int i = 0; i < R; i++) {
+        for (int j = i + 1; j < R; j++) {
+            if (diagonal_elements[i] == diagonal_elements[j]) {
+                are_distinct = false;
+                goto end_check; 
+            }
+        }
+    }
+
+end_check:
+    printf("Output:\n%s\n", are_distinct ? "True" : "False");
+
+    return 0;
+}
